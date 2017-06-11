3836ec52ede250602182b0c66b59bc40d8707093
Diego Zamora <zdr_00@live.com>
2017-06-10 23:10:41 -0500

    home and payments frontend finished

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,28 +12,11 @@ export class HomeComponent {
   amount: number;
   duration: number;
   total: string;
-  fee: number;
-  ntotal: number;
-  stuff: number;
-  display: string;
 
   constructor() {
     this.amount = 150;
     this.duration = 5;
-    this.fee = .016;
-    this.display = ` $ ${this.amount} / ${this.duration} días`;
-    this.total = `$ ${this.amount * (1 + this.fee)} con ${this.fee * 100}%`;
-  }
-
-
-  changeDuration(value1, value2) {
-    this.amount = value1;
-    this.duration = value2;
-    this.ntotal = (value1 * this.fee * value2);
-    this.stuff = this.ntotal + this.amount;
-
-    this.total = `$ ${this.amount * (1 + this.fee)} con ${this.fee * 100}%`;
-    this.display = ` $ ${this.amount} / ${this.duration} días`;
+    
   }
 
   submitLoan () {
