aa8544df00b434daa975ec0fe072fbc452b7228a
Jorge Cuevas <jorge.cuevas.92@gmail.com>
2017-06-13 23:08:29 -0500

    Added Rewards Functionality

--- src/app/components/rewards/rewards.component.ts
+++ src/app/components/rewards/rewards.component.ts
@@ -1,6 +1,6 @@
 import { ElementRef, ViewChild, Renderer2 } from '@angular/core';
 import { Component } from '@angular/core';
-
+declare var swal: any;
 @Component({
    moduleId: module.id,
    selector: 'rewards',
@@ -9,18 +9,26 @@ import { Component } from '@angular/core';
 })
 
 export class RewardsComponent {
-    @ViewChild('dialog_reward') dialog: ElementRef;
-    constructor() {
+    title: String;
 
+    constructor(){
+      this.title = "Sticky";
     }
 
-    ngOnInit() {
 
+    showModal(){
+      swal({
+      title: this.title ,
+      text: "10% de descuento en tu Subway <br> Solo en ITESM Campus Guadalajara <br> - No aplica con otras promociones <br>- No aplica en subway del día <br>Valido hasta 19 de Agosto del 2017 <br> 50 <img style = 'width: 50px; height:50px;'src = './assets/images/KintosCoin_Icon.svg'>",
+      imageUrl: "./assets/images/logo-72.png",
+      showCancelButton: true,
+      confirmButtonColor: "#86C25C",
+      confirmButtonText: "Comprar",
+      cancelButtonText: "Cancelar",
+      html:true
+      });
     }
 
-    showDialog() {
-      console.log('hola');
-      this.dialog.nativeElement.showModal();
-    }
+
 
 }
